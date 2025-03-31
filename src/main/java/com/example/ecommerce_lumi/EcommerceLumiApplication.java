a050397936c7a16bdb1ccae593755533e937ced5
nguyenminhhoi1910 <nguyenminhhoi1910@gmail.com>
2025-03-31 18:39:50 +0700

    init app

---
+++ src/main/java/com/example/ecommerce_lumi/EcommerceLumiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_lumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceLumiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceLumiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ecommerce_lumi/EcommerceLumiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_lumi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceLumiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
